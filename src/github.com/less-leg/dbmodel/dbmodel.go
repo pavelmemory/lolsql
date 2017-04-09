e32752b006a895ecef25b469faa1e18ce68bab09
Pavel <warkille@mail.ru>
2017-04-09 21:13:26 +0300

    MySQL go sql driver added

--- src/github.com/less-leg/dbmodel/dbmodel.go
+++ src/github.com/less-leg/dbmodel/dbmodel.go
@@ -1,9 +1,21 @@
 package dbmodel
 
+import (
+	"fmt"
+)
+
 type Person struct {
 	Id int `lolsql:"id[true]"`
 	Name
 	Password *string `lolsql:"column[SECRET]"`
+}
+
+func (p Person) String() string {
+	pas := ""
+	if p.Password != nil {
+		pas = *p.Password
+	}
+	return fmt.Sprintf(`"Person":{"Id":%d,%s,"Password":"%s"}`, p.Id, p.Name, pas)
 }
 
 func (_ *Person) TableName() string {
@@ -14,4 +26,12 @@ type Name struct {
 	First  string  `lolsql:"column[FIRST_NAME]"`
 	Second *string `lolsql:"column[SECOND_NAME]"`
 	//MiddleName *string `lolsql:"column[MIDDLE_NAME]"`
+}
+
+func (n Name) String() string {
+	sec := ""
+	if n.Second != nil {
+		sec = *n.Second
+	}
+	return fmt.Sprintf(`"Name":{"First":"%s","Second":"%s"}`, n.First, sec)
 }
--- src/github.com/less-leg/main.go
+++ src/github.com/less-leg/main.go
@@ -1,85 +1,99 @@
 package main
 
 import (
-	//_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 
 	"path/filepath"
 
 	"github.com/less-leg/utils"
 	"github.com/less-leg/parser"
 	"github.com/less-leg/sql/generator"
-	"fmt"
-	. "github.com/less-leg/dbmodel/lolsql/djangoadminlog"
-	"github.com/less-leg/dbmodel"
+
 	//"database/sql"
 	"github.com/less-leg/dbmodel/lolsql/person"
+	"database/sql"
+	"log"
 )
 
 func main() {
-	generate := true
+	generate := false
 
 	if generate {
 		packageDir := "github.com/less-leg/dbmodel"
 		sourceDir := "D:/workspace/GoProjects/lolsql/src"
 		parsedStructs := parser.Parse(packageDir, sourceDir)
 
-		fmt.Println("PARSED")
+		log.Println("PARSED")
 		for _, parsedStruct := range parsedStructs {
-			fmt.Printf("%#v\n", parsedStruct)
+			log.Printf("%#v\n", parsedStruct)
 		}
 
-		fmt.Println("DEFINITIONS")
+		log.Println("DEFINITIONS")
 		lolDirPath := utils.RecreateDirectory(filepath.Join(sourceDir, packageDir, "lolsql"))
 		pckgDef := parser.NewPackageDefinition(packageDir, lolDirPath, parsedStructs)
 		for _, strDef := range pckgDef.StructDefinitions {
-			fmt.Printf("%#v\n", strDef)
+			log.Printf("%#v\n", strDef)
 		}
 
 		generator.Generate(pckgDef)
 	} else {
 
 		//ids := []int{10, 102}
-		//fmt.Println(
+		//log.Println(
 		//	person.Select(person.Id(), person.Password()).
 		//	Where(person.IdIs(10).And(person.IdIs(ids[0], ids[1]))).Or(person.PasswordIsNotNull().And(person.PasswordLike("%10", "001_"))).Render())
 
 		//now := time.Now()
-		//fmt.Println(handsome.Select(handsome.Login(), handsome.DateOfBirth()).Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIsNot("LoginIsNot")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now).Or(handsome.SalaryIs(10.2, 100.2).Or(handsome.SalaryIs(-100)))).And(handsome.LoginIs("LoginIs")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIs("LoginIs", "LoginIs2")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIsNot("LoginIsNot", "LoginIsNot2")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginLike("%P1")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginNotLike("%P1")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginLike("LoginLike", "LoginLike2")).Render())
-		//fmt.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginNotLike("LoginNotLike", "LoginNotLike2")).Render())
+		//log.Println(handsome.Select(handsome.Login(), handsome.DateOfBirth()).Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIsNot("LoginIsNot")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now).Or(handsome.SalaryIs(10.2, 100.2).Or(handsome.SalaryIs(-100)))).And(handsome.LoginIs("LoginIs")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIs("LoginIs", "LoginIs2")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginIsNot("LoginIsNot", "LoginIsNot2")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginLike("%P1")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginNotLike("%P1")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginLike("LoginLike", "LoginLike2")).Render())
+		//log.Println(handsome.Select().Where(handsome.DateOfBirthIsNot(&now)).Or(handsome.SalaryIs(10.2, 100.2)).And(handsome.LoginNotLike("LoginNotLike", "LoginNotLike2")).Render())
 
-		//db, err := sql.Open("mysql", "root:root@/akeos?parseTime=true")
-		//utils.PanicIf(err)
-		//defer db.Close()
+		db, err := sql.Open("mysql", "root:root@/Development?parseTime=true")
+		utils.PanicIf(err)
+		defer db.Close()
 		////
 		//dals, err := djangoadminlog.Select().Where(djangoadminlog.ObjectReprLike("Harry%", "Lol%").And(djangoadminlog.ActionFlagIs(1)).And(djangoadminlog.ActionFlagIs(1))).Fetch(db)
-		fmt.Println(
-			Select(ChangeMessage(), ActionTime()).
-				Where(ObjectReprIs("Harry%", "Lol%").And(ActionFlagIs(1)).And(ActionFlagIs(1))).And(AskPasswordIs(dbmodel.No)).
-				Render(),
-		)
-		//fmt.Println(len(dals))
+		//log.Println(
+		//	Select(ChangeMessage(), ActionTime()).
+		//		Where(ObjectReprIs("Harry%", "Lol%").And(ActionFlagIs(1)).And(ActionFlagIs(1))).And(AskPasswordIs(dbmodel.No)).
+		//		Render(),
+		//)
+		//log.Println(len(dals))
 		//var db *sql.DB
 		// Select().Where(AskPasswordIs(dbmodel.Yes)).Fetch(db)
-		//fmt.Println(len(dals))
+		//log.Println(len(dals))
 
 		//ps, err := person.Select().Fetch(db)
-		//fmt.Println(err, ps)
+		//log.Println(err, ps)
 		//
 
 		//sqlRend := person.Select(person.Id(), person.Password()).
-		sqlRend := person.Select(person.Name_First()).
-			Where(person.IdIs(10).And(person.IdIs(10, 1000))).
-				Or(person.PasswordIsNotNull().And(person.PasswordLike("%10", "001_"))).
-				Or(person.FirstNotLike("Pavrl")).
-				Or(person.SecondIsNull()).
-			Render()
-		fmt.Println(sqlRend)
+		//sqlRend := person.Select(person.Name_First()).
+		//	Where(person.IdIs(10).And(person.IdIs(10, 1000))).
+		//		Or(person.PasswordIsNotNull().And(person.PasswordLike("%10", "001_"))).
+		//		Or(person.FirstNotLike("Pavrl")).
+		//		Or(person.SecondIsNull()).
+		//	Render()
+		//log.Println(sqlRend)
+
+		persons, err := person.Select().
+			//Where(person.IdIs(10).And(person.IdIs(10, 1000))).
+			//Or(person.PasswordIsNotNull().And(person.PasswordLike("%10", "001_"))).
+			//Or(person.FirstNotLike("Pavrl")).
+			//Or(person.SecondIsNull()).
+			Fetch(db)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for _, per := range persons {
+			log.Println(per)
+		}
+
 
 		//ps, err := person.Select(person.Id(), person.Password()).
 		//	Where(person.IdIs(10).And(
@@ -89,12 +103,12 @@ func main() {
 		//).Fetch(db)
 
 		//r,e := db.Query("select Id, SECRET from PERSONS where SECRET like ?", 10)
-		//fmt.Println(e, r)
+		//log.Println(e, r)
 		//
 		//
-		//fmt.Println(person.Select(person.Id(), person.Password()).Where(person.PasswordLike("%")).Render())
+		//log.Println(person.Select(person.Id(), person.Password()).Where(person.PasswordLike("%")).Render())
 		//ps, err := person.Select(person.Id(), person.Password()).Where(person.PasswordLike("%")).Fetch(db)
 		//
-		//fmt.Println(err, ps)
+		//log.Println(err, ps)
 	}
 }
